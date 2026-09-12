a7e7df5855638e63f6aeac24c809a59b4071e1ea
suraj <suraj87parmar@gmail.com>
2026-09-12 20:19:01 +0530

    wolf

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,7 @@ import ReelsGrid from "@/components/ReelsGrid"
 
 export default async function SearchPage() {
   const reels = await getReels();
-
+  console.log("wolverine")
   return (
     <div className="max-w-3xl mx-auto p-4 pb-24">
       <h1 className="text-xl font-bold mb-4">Explore Reels</h1>
